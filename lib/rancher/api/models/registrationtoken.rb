824d7cf49e5f77d8964b2bc4716ff48064a4b924
Henry Ho <henry@avvo.com>
2016-08-23 15:51:33 -0700

    Do not use custom collection path.

--- lib/rancher/api/models/registrationtoken.rb
+++ lib/rancher/api/models/registrationtoken.rb
@@ -5,7 +5,6 @@ module Rancher
       include Helpers::Model
 
       belongs_to :project
-      collection_path "projects/:project_id/registrationtokens" # FIXME: fork her gem and pull in fix for this: https://github.com/remiprev/her/issues/224
     end
   end
 end
